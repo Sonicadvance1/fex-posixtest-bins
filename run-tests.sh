948eb5308d560149824bc25736b3432a3e68cd3c
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-05-01 14:17:23 +0300

    runner: Add timeout to skip to next text

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,6 @@ shopt -s globstar
 echo log start > $2
 for i in **/*.test; do # Whitespace-safe and recursive
     echo running "$i"
-    ${1}"$i"
+    timeout 45s ${1}"$i"
     echo "$i" $? >> ${2}
 done
